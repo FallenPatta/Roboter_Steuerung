efe1d912638855e44dfea39219b9f549c6cb2107
David Bohlig <d-b-01@live.de>
2017-08-10 11:17:09 +0200

    interrupts

--- Robot_Steuerung.ino
+++ Robot_Steuerung.ino
@@ -1,9 +1,18 @@
-const int pin = 13;
+int left_ticks = 0;
+int left_direction = 1;
 
-void toggle() {
-  static int state = 0;
-  state = !state;
-  digitalWrite(BUILTIN_LED, state);
+int right_ticks = 0;
+int right_direction = 1;
+
+long stoptime = 0;
+bool driving_status = true;
+
+void tick_left_ISR() {
+  left_ticks += left_direction;
+}
+
+void tick_right_ISR() {
+  right_ticks += right_direction;
 }
 
 void setup() {
@@ -12,101 +21,65 @@ void setup() {
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
-  pinMode(D5, INPUT);
-  pinMode(D6, INPUT);
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(D3, HIGH);
   digitalWrite(D4, LOW);
   digitalWrite(BUILTIN_LED, LOW);
-    for(int i = 0; i<10; i++){
-      digitalWrite(BUILTIN_LED, HIGH);
-      delay(100);
-      digitalWrite(BUILTIN_LED, LOW);
-      delay(100);
-    }
-    Serial.begin(115200);
-    Serial.println("START");
-    Serial.println(D1);
-    Serial.println(D2);
-    Serial.println(D3);
-    Serial.println(D4);
-    Serial.println(D5);
-    Serial.println(D6);
-//    Serial.println(BUILTIN_LED);
-//  pinMode(pin, INPUT);
-//  attachInterrupt(pin, toggle, CHANGE);
-//  toggle();
+
+  pinMode(D5, INPUT);
+  attachInterrupt(D5, tick_left_ISR, CHANGE);
+  pinMode(D6, INPUT);
+  attachInterrupt(D6, tick_right_ISR, CHANGE);
+
+  Serial.begin(115200);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(100);
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(100);
+  }
+  stoptime = millis();
+  Serial.println("START");
 }
 
-bool last_left = 0;
-int left_ticks = 0;
-long stoptime = 0;
-bool driving_status = true;
+ int relative_position = 48;
 void loop() {
-  if(last_left != digitalRead(D5)){
-    last_left = !last_left;
-    left_ticks++;
-    Serial.println(left_ticks); 
-  }
-  if(left_ticks < 200){
+
+    Serial.print("ticks (l,r): ");
+    Serial.print(left_ticks);
+    Serial.print(", ");
+    Serial.println(right_ticks);
+  
+
+  if (driving_status)
+  {
     digitalWrite(D3, HIGH);
     digitalWrite(D4, LOW);
-    analogWrite(D1, 1024);
-    analogWrite(D2, 1024);
-    driving_status = true;
-  } else if (driving_status) {
+    analogWrite(D1, 950);
+    analogWrite(D2, 950);
+  }
+  else
+  {
     digitalWrite(D3, LOW);
     digitalWrite(D4, LOW);
     analogWrite(D1, 0);
     analogWrite(D2, 0);
-    stoptime = millis();
+  }
+
+  if(left_ticks < relative_position && right_ticks < relative_position)
+  {
+    driving_status = true;
+  }
+  else 
+  {
     driving_status = false;
   }
-    
-  if(!driving_status && millis() - stoptime >= 10000){
-    left_ticks = 0;
+
+  if (millis() - stoptime >= 5000)
+  {
+    while(left_ticks >= relative_position) left_ticks -= relative_position;
+    while(right_ticks >= relative_position) right_ticks -= relative_position;
+    stoptime += 5000;
   }
-//  digitalWrite(D3, HIGH);
-//  digitalWrite(D4, LOW);
-//    analogWrite(D1, 900);
-//    analogWrite(D2, 900);
-//    delay(250);
-//    digitalWrite(D3, LOW);
-//    digitalWrite(D4, LOW);
-//    analogWrite(D1, 0);
-//    analogWrite(D2, 0);
-//    delay(250);
-//    
-//    digitalWrite(D3, LOW);
-//    digitalWrite(D4, HIGH);
-//    analogWrite(D1, 900);
-//    analogWrite(D2, 900);
-//    delay(250);
-//    digitalWrite(D3, LOW);
-//    digitalWrite(D4, LOW);
-//    analogWrite(D1, 0);
-//    analogWrite(D2, 0);
-//    delay(250);
-//    
-//      digitalWrite(D3, HIGH);
-//  digitalWrite(D4, HIGH);
-//    analogWrite(D1, 900);
-//    analogWrite(D2, 900);
-//    delay(250);
-//    digitalWrite(D3, LOW);
-//    digitalWrite(D4, LOW);
-//    analogWrite(D1, 0);
-//    analogWrite(D2, 0);
-//    delay(250);
-//    
-//    digitalWrite(D3, LOW);
-//    digitalWrite(D4, LOW);
-//    analogWrite(D1, 900);
-//    analogWrite(D2, 900);
-//    delay(250);
-//    digitalWrite(D3, LOW);
-//    digitalWrite(D4, LOW);
-//    analogWrite(D1, 0);
-//    analogWrite(D2, 0);
-//    delay(10000);
-  }
+  
+}
