828dabf241c1f263c58fc1d98aaf0956f9aeb433
David Bohlig <d-b-01@live.de>
2017-09-09 15:44:11 +0200

    schedul und Stuerung

--- Robot_Steuerung.ino
+++ Robot_Steuerung.ino
@@ -14,11 +14,10 @@
 #define ROBOT_WIDTH 13.5f
 #define ROBOT_WIDTH_2 ROBOT_WIDTH/2.0f
 
-#define TICK_TRUST 0.3f
+#define TICK_TRUST 0.1f
 
 #define RINGBUFFERSIZE 128
 
-//#define NOMOTORS
 //ESP.getFreeHeap() // freier Arbeitsspeicher ##########>>> NICHT löschen, das brauchst du sowieso wieder! <<<########## 
 
 const String commands[] = {"SendStatus", "Ok", "TurnOn"};
@@ -93,20 +92,11 @@ int clamp(int a, int minval, int maxval){
   return a;
 }
 
-void calcSpeeds(double dT){
-  static float last_ticks_l = 0;
-  static float last_ticks_r = 0;
-  wheel_speeds[0] = (1.0f-TICK_TRUST) * wheel_speeds[0] 
-                    + TICK_TRUST * (TICK_LEN*(left_ticks - last_ticks_l)/dT);
-  last_ticks_l = left_ticks;
-    
-  wheel_speeds[1] = (1.0f-TICK_TRUST) * wheel_speeds[1] 
-                    + TICK_TRUST * (TICK_LEN*(right_ticks - last_ticks_r)/dT);
-  last_ticks_r = right_ticks;
-    
+int sign(int num){
+  return num>=0?1:-1;
 }
 
-int sign(int num){
+int sign(float num){
   return num>=0?1:-1;
 }
 
@@ -166,212 +156,11 @@ void setMotors(int val1, int val2) {
 //#endif
 }
 
-void speedControl(float dT, float vSoll, float rSoll, int* output) {
-  static float integral_part_l = 0;
-  static float integral_part_r = 0;
-  static float last_error_l = 0;
-  static float last_error_r = 0;
-
-  float k_p = 10.0; //3
-  float k_d = 10.0; //3 
-  float k_i = 0.01; //0.01
-
-  bool switch_sides = rSoll < 0;
-
-  float radius = fabs(rSoll);
-  
-  float rSollMax = radius+ROBOT_WIDTH_2;
-  float rSollMin = radius-ROBOT_WIDTH_2;
-  float vInner = vSoll * (rSollMin / radius);
-  float vAusser = vSoll * (rSollMax / radius);
-
-  if(abs(rSoll) - ROBOT_WIDTH_2 <= 0.1 ){
-    vInner = 0;
-    vAusser = 2.0f*vSoll;
-  }
-  if(abs(rSoll) >= 300){
-    vInner = vSoll;
-    vAusser = vSoll;
-  }
-  
-  float vLinks = vAusser;
-  float vRechts = vInner;
-  
-  if(switch_sides){
-    vLinks = vInner;
-    vRechts = vAusser;
-  }
-
-  float error_l = vLinks - wheel_speeds[0];
-  float error_r = vRechts - wheel_speeds[1];
-
-  float pVal_l = error_l * k_p;
-  float dVal_l = (error_l-last_error_l) * k_d;
-  integral_part_l += error_l * k_i;
-  last_error_l = error_l;
-
-  
-  float pVal_r = error_r * k_p;
-  float dVal_r = (error_r-last_error_r) * k_d;
-  integral_part_r += error_r * k_i;
-  last_error_r = error_r;
-
-  integral_part_l = clamp(integral_part_l, -200, 200);
-  integral_part_r = clamp(integral_part_r, -200, 200);
-
-//  Serial.print("p: ");
-//  Serial.print(pVal_l);
-//  Serial.print("i: ");
-//  Serial.print(integral_part_l);
-//  Serial.print("d: ");
-//  Serial.print(dVal_l);
-//  Serial.println();
-
-  output[0] = (int)(pVal_l - dVal_l + integral_part_l);
-  output[1] = (int)(pVal_r - dVal_r + integral_part_r);
-}
-
-
-float left_tps = 0;
-float right_tps = 0;
-float iVal_left = 0;
-float iVal_right = 0;
-bool wheelSpeedControl(char wheel, float ticksPerSecond, float dT, float * correction, float clampAroundZero) {
-  float proportional = 20.0;
-  float differential = 3.0;
-  float integral = 0.001;
-
-  if (clampAroundZero < 0) {
-    clampAroundZero = -1 * clampAroundZero;
-  }
-
-  int wheelTicks = 0;
-  float tps_last = 0;
-  float iVal = 0;
-  long * timesArray;
-  int index;
-  switch (wheel) {
-    case 'A':
-    case 'a':
-      tps_last = left_tps;
-      wheelTicks = left_ticks;
-      iVal = iVal_left;
-      timesArray = left_ticks_times;
-      index = left_times_index;
-      break;
-    case'B':
-    case'b':
-      tps_last = right_tps;
-      wheelTicks = right_ticks;
-      iVal = iVal_right;
-      timesArray = right_ticks_times;
-      index = right_times_index;
-      break;
-    default:
-      break;
-  }
-
-  float tps = 1000.0f / ((float)(timesArray[(index - 1) % TICKTIMESSIZE] - timesArray[(index)]) / (float)TICKTIMESSIZE); //tick_diff/dT;
-  float dError = tps - tps_last;
-  float error = ticksPerSecond - tps;
-
-  float pVal = proportional * error;
-  float dVal = differential * dError;
-  iVal += integral * error;
-
-  iVal = clamp(iVal, -70, 70);
-
-  *correction = clamp( pVal - dVal + iVal,-1 * clampAroundZero,clampAroundZero);
-
-  switch (wheel) {
-    case 'A':
-    case 'a':
-      iVal_left = iVal;
-      left_tps = tps;
-      break;
-    case'B':
-    case'b':
-      right_tps = tps;
-      iVal_right = iVal;
-      break;
-    default:
-      break;
-  }
-  return true;
-}
-
-void ticksControl(int speed1, int speed2, int* output) {
-
-  float proportional = 7.0;
-  float differential = 25.0;
-  float integral = 0.02;
-
-  int tick_diff = (left_ticks - right_ticks);
-  int tick_diff_diff = tick_diff - tick_difference;
-  tick_difference = tick_diff;
-
-  float proportional_tickcontrol = proportional * tick_diff;
-  integral_tickcontrol += integral * tick_diff;
-  float differential_tickcontrol = differential * tick_diff_diff;
-
-  float control_output = clamp(proportional_tickcontrol - differential_tickcontrol + integral_tickcontrol, -200, 200);
-
-  output[0] = clamp(speed1 - control_output, 0, 1024);
-  output[1] = clamp(speed2 - control_output, 0, 1024);
-
-  Serial.print("pid:\t");
-  Serial.print(proportional_tickcontrol);
-  Serial.print(",\t");
-  Serial.print(differential_tickcontrol);
-  Serial.print(",\t");
-  Serial.print(integral_tickcontrol);
-  Serial.print(",\t");
-  Serial.print(output[0]);
-  Serial.print(",\t");
-  Serial.print(output[1]);
-  Serial.print(",\t");
-  Serial.print(tick_diff);
-  Serial.print(",\t");
-  Serial.println(tick_diff_diff);
-}
-
-//Ab hier: WIFI
-void wifiSetup() {
-//  WiFi.mode(WIFI_AP_STA);
-//  WiFi.softAPConfig(localip, gateway, subnet);
-//  WiFi.softAP(ssid);
-//  IPAddress myIP = WiFi.softAPIP();
-  WiFi.mode(WIFI_STA);
-  //WiFi.config(localip, gateway, subnet);
-  WiFi.begin("Ultron", "Zu120%SICHERundMITsicherMEINichSICHER");
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed");
-    while (true) {}
-  }
-
-  Serial.println("Connected.");
-  Serial.print("MAC: ");
-  Serial.println(WiFi.macAddress());
-  Serial.print("IP:  ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Subnet: ");
-  Serial.println(WiFi.subnetMask());
-  Serial.print("Gateway: ");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("DNS: ");
-  Serial.println(WiFi.dnsIP());
-  Serial.print("Channel: ");
-  Serial.println(WiFi.channel());
-  Serial.print("Status: ");
-  Serial.println(WiFi.status());
-  server.begin();
-}
-
 //Ab hier: Tasks
 
-WiFiClient working_client;
-float kurvenradius = 0.0f;
-float geschwindigkeit = 0.0f;
+WiFiClient * working_client = new WiFiClient();
+float kurvenradius = 30.0f;
+float geschwindigkeit = 10.0f;
 bool forward = true;
 
 uint8_t * data_ring_buffer = new uint8_t[RINGBUFFERSIZE]();
@@ -461,60 +250,223 @@ void frameToInstructions(uint8_t * frame, int frame_size){
   }
   geschwindigkeit = instructions[0].toFloat();//0;//
   kurvenradius = instructions[1].toFloat();//0;//
-//  Serial.print("G: ");
-//  Serial.print(geschwindigkeit);
-//  Serial.print(".");
-//  Serial.print("R: ");
-//  Serial.println(kurvenradius);
+  Serial.print("G: ");
+  Serial.print(geschwindigkeit);
+  Serial.print(".");
+  Serial.print("R: ");
+  Serial.println(kurvenradius);
   delete [] instructions;
 }
 
+//Feedback
+int calcSpeeds(){
+  double * dT = (double *) task_scheduler.functionalMem[0];
+  double dT_sec = (*dT)/1000.0;
+  static int last_ticks_l = 0;
+  static int last_ticks_r = 0;
+  float dTicks_l = (left_ticks - last_ticks_l) * sign(motor_speeds[0]);
+  float dTicks_r = (right_ticks - last_ticks_r) * sign(motor_speeds[1]);
+  wheel_speeds[0] = (1.0f-TICK_TRUST) * wheel_speeds[0] 
+                    + TICK_TRUST * (TICK_LEN*dTicks_l/dT_sec);
+  last_ticks_l = left_ticks;
+    
+  wheel_speeds[1] = (1.0f-TICK_TRUST) * wheel_speeds[1] 
+                    + TICK_TRUST * (TICK_LEN*dTicks_r/dT_sec);
+  last_ticks_r = right_ticks;
+
+  Serial.print(wheel_speeds[0], DEC);
+  Serial.print(" - ");
+  Serial.println(wheel_speeds[1], DEC);
+
+  return 0;
+}
+
+//Regelung
+void speedControl(float dT, float vSoll, float rSoll, int* output) {
+  static float integral_part_l = 0;
+  static float integral_part_r = 0;
+  static float last_speed_l = 0;
+  static float last_speed_r = 0;
+
+  float k_p = 20.0;//20.0; //3
+  float k_d = 0.0;
+  float k_i = 1.0;//1.0;//10.0
+
+  bool switch_sides = rSoll < 0;
+
+  float radius = fabs(rSoll);
+  
+  float rSollMax = radius+ROBOT_WIDTH_2;
+  float rSollMin = radius-ROBOT_WIDTH_2;
+  float vInner = vSoll * (rSollMin / radius);
+  float vAusser = vSoll * (rSollMax / radius);
+
+  if(abs(rSoll) - ROBOT_WIDTH_2 <= 0.1 ){
+    vInner = 0;
+    vAusser = 2.0f*vSoll;
+  }
+  if(abs(rSoll) >= 300){
+    vInner = vSoll;
+    vAusser = vSoll;
+  }
+  
+  float vLinks = vAusser;
+  float vRechts = vInner;
+  
+  if(switch_sides){
+    vLinks = vInner;
+    vRechts = vAusser;
+  }
+
+  float error_l = vLinks - wheel_speeds[0];
+  float dError_l = wheel_speeds[0] - last_speed_l;
+  float error_r = vRechts - wheel_speeds[1];
+  float dError_r = wheel_speeds[1] - last_speed_r;
+
+  float pVal_l = vLinks * k_p;
+  float dVal_l = dError_l * k_d;
+  integral_part_l += error_l * k_i;
+  last_speed_l = wheel_speeds[0];
+
+  
+  float pVal_r = vRechts * k_p;
+  float dVal_r = dError_r * k_d;
+  integral_part_r += error_r * k_i;
+  last_speed_r = wheel_speeds[1];
+
+  output[0] = (int)(pVal_l - dVal_l + integral_part_l);
+  output[1] = (int)(pVal_r - dVal_r + integral_part_r);
+}
+
 int comReadoutTask(){
-  uint8_t * frame = NULL;
-  int * frame_size = NULL;
-  if(bufferGetFrame(&frame, &frame_size)){
-    frameToInstructions(frame, *frame_size);
-    free(frame);
-    free(frame_size);
+  if(working_client && working_client->connected()){
+    uint8_t * frame = NULL;
+    int * frame_size = NULL;
+    if(bufferGetFrame(&frame, &frame_size)){
+      frameToInstructions(frame, *frame_size);
+      free(frame);
+      free(frame_size);
+    }
+  }
+  else{
+    if(!working_client || !working_client->connected()){
+      kurvenradius = 0;
+      geschwindigkeit = 0;
+      setMotors(0,0);
+    }
   }
   return 0;
 }
 
+//Crasht bei niedrigem Batteriestand
 int comTask(){
-  
-  if(!working_client || !working_client.connected()){
-    working_client = server.available();
+//  Serial.println("1");
+  if(!working_client || !working_client->connected()){
+    *working_client = server.available();
   }
-  
-  if(working_client.connected()){
-    if(working_client.available()){
-      int available_bytes = working_client.available();
+//  Serial.println("2");
+  if(working_client->connected()){
+//    Serial.println("3");
+    if(working_client->available()){
+//      Serial.println("4");
+      int available_bytes = working_client->available();
       uint8_t * raw = (uint8_t*)malloc((available_bytes)*sizeof(uint8_t));
-      working_client.read(raw, available_bytes);
+      working_client->read(raw, available_bytes);
+//      Serial.println("5");
       writeToBuffer(raw, available_bytes);
       free(raw);
-      working_client.println(".");
-      working_client.flush();
+//      Serial.println("6");
+      working_client->println(".");
+      //working_client->flush();
 //      Serial.println(ESP.getFreeHeap());
 //      for(int i = 0; i <= RINGBUFFERSIZE; i++){
 //        Serial.print((char)data_ring_buffer[i]);
 //      }
 //      Serial.println();
+//      Serial.println("7");
     }
   }
   return 0;
 }
 
 int motorControlTask(){
-    calcSpeeds(0.150f);
-    int * cVal = new int[2];
-    speedControl(1.0f, geschwindigkeit, kurvenradius, cVal);
-    motor_setSpeeds[0] = clamp(motor_setSpeeds[0]+cVal[0], 0, 1024);
-    motor_setSpeeds[1] = clamp(motor_setSpeeds[1]+cVal[1], 0, 1024);
-    delete [] cVal;
+  
+    double * dT = (double *) task_scheduler.functionalMem[0];
+    double dT_sec = (*dT)/1000.0;
+  
+    static int * cVal = new int[2];
+    static int * cVal_last = new int[2];
+    cVal_last[0] = cVal[0];
+    cVal_last[1] = cVal[1];
+    speedControl(dT_sec, geschwindigkeit, kurvenradius, cVal);
+
+    //Restrict acceleration
+    int * d_cVal = new int[2];
+    d_cVal[0] = cVal[0] - cVal_last[0];
+    d_cVal[1] = cVal[1] - cVal_last[1];
+//    d_cVal[0] = clamp(d_cVal[0], -50, 50);
+//    d_cVal[1] = clamp(d_cVal[1], -50, 50);
+    
+    cVal[0] = cVal_last[0] + d_cVal[0];
+    cVal[1] = cVal_last[1] + d_cVal[1];
+    motor_setSpeeds[0] = clamp(cVal[0], 0, 1024); //motor_setSpeeds[0]+
+    motor_setSpeeds[1] = clamp(cVal[1], 0, 1024); //motor_setSpeeds[1]+
+    delete [] d_cVal;
+
+    if(geschwindigkeit == 0){
+      motor_setSpeeds[0] = 0;
+      motor_setSpeeds[1] = 0;
+    }
     setMotors(motor_setSpeeds[0], motor_setSpeeds[1]);
-    stoptime2 += 150;
+//    Serial.print("(");
+//    Serial.print(motor_setSpeeds[0], DEC);
+//    Serial.print(",");
+//    Serial.print(motor_setSpeeds[1], DEC);
+//    Serial.println(")");
+//    Serial.println(ESP.getFreeHeap());
     return 0;
+}
+
+//Ab hier: WIFI
+void wifiSetup() {
+  Serial.println("Setting up WiFI");
+//  WiFi.mode(WIFI_AP_STA);
+//  WiFi.softAPConfig(localip, gateway, subnet);
+//  WiFi.softAP(ssid);
+//  IPAddress myIP = WiFi.softAPIP();
+  WiFi.mode(WIFI_STA);
+  //WiFi.config(localip, gateway, subnet);
+  WiFi.begin("Ultron", "Zu120%SICHERundMITsicherMEINichSICHER");
+  delay(1000);
+//  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+//    Serial.println("Connection Failed");
+//    while (true) {
+//      Serial.println("Connection Failed");
+//      }
+//  }
+
+  Serial.println("Connected.");
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("IP:  ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Subnet: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("DNS: ");
+  Serial.println(WiFi.dnsIP());
+  Serial.print("Channel: ");
+  Serial.println(WiFi.channel());
+  Serial.print("Status: ");
+  Serial.println(WiFi.status());
+  server.begin();
+
+  //Attach Tasks
+  String comTaskName = "communication";
+  task_scheduler.addFunction(&comTask, comTaskName, 10, millis(), 5, 0, 0);
+  String comReadoutTaskName = "comReadout";
+  task_scheduler.addFunction(&comReadoutTask, comReadoutTaskName, 1, millis(), 7, 0, 0);
 }
 
 //Ab hier: Setup und main Loop
@@ -537,16 +489,6 @@ void setup() {
 
   //COM SETUP
   Serial.begin(115200);
-  wifiSetup();
-
-  //TASKS SETUP
-  stoptime = millis();
-  String motorctrlTaskName = "motor-ctrl";
-  task_scheduler.addFunction(&motorControlTask, motorctrlTaskName, 150, millis(), 10, 0, 0);
-  String comTaskName = "communication";
-  task_scheduler.addFunction(&comTask, comTaskName, 1, millis(), 10, 0, 0);
-  String comReadoutTaskName = "comReadout";
-  task_scheduler.addFunction(&comReadoutTask, comReadoutTaskName, 1, millis(), 7, 0, 0);
 
   //DEFAULTS EINSTELLEN
   setMotors(0, 0);
@@ -562,10 +504,25 @@ void setup() {
     delay(100);
   }
   Serial.println("START");
+
+  //TASKS SETUP
+  wifiSetup();
+  String motorctrlTaskName = "motor-ctrl";
+  double * arg_0 = new double();
+  *arg_0 = 150.0;
+  int ** args_0 = new int*[1];
+  args_0[0] = (int*)arg_0;
+  task_scheduler.addFunction(&motorControlTask, motorctrlTaskName, *arg_0, millis(), 10, args_0, 1);
+
+  
+  String speedUpdateTaskName = "motor-speed";
+  double * arg_1 = new double();
+  *arg_1 = 30.0;
+  int ** args_1 = new int*[1];
+  args_1[0] = (int*)arg_1;
+  task_scheduler.addFunction(&calcSpeeds, speedUpdateTaskName, *arg_1, millis(), 50, args_1, 1);
 }
 
 void loop() {
   task_scheduler.execute();
-  delay(1);
-
 }
